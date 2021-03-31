6515e582422ce78e42340d878458002f175ac96f
csDevin <1561472800@qq.com>
2021-04-01 00:53:28 +0800

    some of the backTrack

---
+++ code/20210331_Back_Tracking/Leetcode_46_Permutations.cpp
@@ -0,0 +1,101 @@
+// L46: Permutations (Medium) 全排列的结果是n!
+// 时间复杂度分析
+// 1. backtrack（dfs函数） 的调用次数是O(n!)级别的。
+// 2. 对于 backtrack 调用的每个叶结点（共 n!个），我们需要将当前答案使用 O(n) 的时间复制到答案数组中，相乘得时间复杂度为 O(n×n!)。
+// 因此综合1，2，两者取其小者，时间复杂度为 O(n×n!)。
+// 空间复杂度：O(n×n!)，全排列个数n！，每个全排列所占空间为n，结果n*n!
+
+
+
+// 自己的代码：较为复杂
+// 思路：递归深度有且只有nums.size()层，使用一个深度为nums.size()的栈，每次dfs一个元素时，将这个元素压入栈中。
+// 当temp存储的元素个数达到nums.size()个时，我们将temp push_back到result中，作为一种排列情况。
+// 记住，在此次dfs结束之前需要使用pop_back()函数取出栈顶元素，以便上一层递归去寻找其他可能的答案。
+// 回溯法总结：有visited[i]=1,在dfs之后就要有，visited[i]=0；有temp.push_back(nums[x]),就要有temp.popback()。
+// 只有这样，才能达到回溯的效果，从而去回到上一层递归的状态！从而可以去寻找其他的可能的结果。
+// void dfs(vector<int>& nums, vector<int>& visited, vector<vector<int>>& result, vector<int>& temp, int x)
+// {
+//     vector<int> v;
+//     temp.push_back(nums[x]);  // nums[x]写成了x
+//     if(temp.size()==nums.size())
+//     {
+//         result.push_back(temp);
+//     }
+//     for(int i=0; i<nums.size(); i++)  //***此函数中的所有的i全部写成了x！！！
+//     {
+//         if(visited[i]==1)
+//             continue;
+//         visited[i]=1;
+//         dfs(nums, visited, result, temp, i);
+//         visited[i]=0;
+//     }
+//     temp.pop_back();  //***只记得要push_back()了，忘记pop_back了
+//     return;
+// }
+
+// vector<vector<int>> permute(vector<int>& nums) {
+//     int len = nums.size();
+//     vector<vector<int>> result;
+//     vector<int> visited(len);
+//     vector<int> temp;
+//     for(int i=0; i<nums.size(); i++)
+//     {
+//         visited[i]=1;
+//         dfs(nums, visited, result, temp,i);  // 主函数dfs前后没有加visited
+//         visited[i]=0;
+//     }
+//     return result;
+// }
+
+
+
+// better code
+// visited + depth + result
+// visited记录已经访问过的元素，depth记录当前递归的深度，result记录栈中元素，在此题中depth==result.size()
+// 执行结果：通过
+// 执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+// 内存消耗：7.4 MB, 在所有 C++ 提交中击败了80.59%的用户
+
+void dfs(vector<int>& nums, int x, vector<int>& visited, vector<int>& temp, vector<vector<int>>& result)
+{
+    temp.push_back(nums[x]);  //***nums[x]写成了x
+    if(temp.size() == nums.size())
+    {
+        result.push_back(temp);
+    }
+    for(int i=0; i<nums.size(); i++)
+    {
+        if(visited[i]==0)
+        {
+            visited[i]=1;
+            dfs(nums, i, visited, temp, result);
+            visited[i]=0;
+        }  
+    }
+    temp.pop_back();
+    return;
+}
+
+vector<vector<int>> permute(vector<int>& nums) {
+    int len = nums.size();
+    vector<int> visited(len);
+    vector<int> temp;
+    vector<vector<int>> result;
+    
+    for(int i=0; i<len; i++)
+    {
+        visited[i]=1;
+        dfs(nums, i, visited, temp, result);
+        visited[i]=0;
+
+    }
+    return result;
+}
+
+int main()
+{
+    vector<int> vec = {1, 2, 3};
+    permute(vec);
+    int a = 2;
+    return 0;
+}
---
+++ code/20210331_Back_Tracking/Leetcode_77_Combinations.cpp
@@ -0,0 +1,111 @@
+// L77: Combinations (Medium)
+// 时间复杂度：
+// 空间复杂度：
+
+#include<bits/stdc++.h>
+using namespace std;
+// 稍显复杂：带visited的写法
+void dfs(int n, int k, int i, vector<int>& visited, vector<int>& temp, vector<vector<int>>& result)
+{
+    temp.push_back(i);
+    if(temp.size() == k)  // ==k就应该return不然会导致无用的搜索，如temp.size()>k的搜索。
+    {
+        result.push_back(temp);
+        temp.pop_back();
+        return;
+    }
+    for(int j=i+1; j<=n; j++)
+    {
+        if(visited[j]==0)
+        {
+            visited[j]=1;
+            dfs(n, k, j, visited, temp, result);
+            visited[j]=0;
+        }
+    }
+    temp.pop_back();
+    return;
+}
+
+vector<vector<int>> combine(int n, int k) {
+    vector<int> visited(n+1);
+    //***范围是[1, n]左闭右闭的区间，visited(n+1)写成了visited(n)。
+    vector<int> temp;        
+    vector<vector<int>> result;
+    for(int i=1; i<=n; i++)  // 因为是递增序列，无需遍历i=n;
+    // 范围是[1, n]左闭右闭的区间，i=1写成了i=0。
+    {
+        visited[i] = 1;
+        dfs(n, k, i, visited, temp, result);
+        visited[i] = 0;
+    }
+    return result;
+}
+
+
+
+
+
+// 代码优化（不带visited的写法）
+// 上述代码你去仔细思考是不需要带visited的，因为每次dfs你都选择了i+1到n的数，
+// 每次选择都是比i大的数，也就是说i不在选择区间内，也就不存在重复问题。
+void dfs(int n, int k, int i, vector<int>& temp, vector<vector<int>>& result)
+{
+    temp.push_back(i);
+    if(temp.size() == k)  // ==k就应该return不然会导致无用的搜索，如temp.size()>k的搜索。
+    {
+        result.push_back(temp);
+        temp.pop_back();
+        return;
+    }
+    for(int j=i+1; j<=n; j++)
+        dfs(n, k, j, temp, result);
+    temp.pop_back();
+    return;
+}
+vector<vector<int>> combine(int n, int k) {
+    vector<int> temp;        
+    vector<vector<int>> result;
+    for(int i=1; i<=n; i++)  // 因为是递增序列，无需遍历i=n;
+    // 范围是[1, n]左闭右闭的区间，i=1写成了i=0。
+    dfs(n, k, i, temp, result);
+    return result;
+}
+
+
+
+
+
+// 组合型枚举(回溯枚举)（不带visited的写法）
+// 思路：对于每一个属于[1,n]的元素i，考虑选择i和不选择i这两种情况，分别递归，直到temp.size()==k;
+void dfs(int n, int k, int x, vector<int>& temp, vector<vector<int>>& result)
+{
+    if(temp.size() == k)  //***应该先判断size==k再判断越界>n，顺序很重要，写反了
+    {
+        result.push_back(temp);
+        return;
+    }
+    if(x>n)  //***应该先判断size==k再判断越界>n，顺序很重要，写反了
+        return;
+    temp.push_back(x);
+    dfs(n, k, x+1, temp, result);  // 枚举中包含x元素
+    temp.pop_back();
+    dfs(n, k, x+1, temp, result);  // 枚举中不包含x元素
+    return;
+}
+
+vector<vector<int>> combine(int n, int k) {
+    vector<int> temp;
+    vector<vector<int>> result;
+    dfs(n, k, 1, temp, result);
+    return result;
+}
+
+int main()
+{
+    // vector<int> vec = {1, 2, 3};
+    int n = 4, k = 2;
+    combine(n, k);
+    int a = 2;
+    return 0;
+}
