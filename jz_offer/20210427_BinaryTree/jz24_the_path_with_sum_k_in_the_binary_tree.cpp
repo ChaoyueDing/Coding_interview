72a1ee1c8a0c16c91b9c6677376bdf348cc3bcfa
csDevin <1561472800@qq.com>
2021-04-27 21:57:13 +0800

    jz24_path_sum

--- jz_offer/20210427_BinaryTree/jz24_the_path_with_sum_k_in_the_binary_tree.cpp
+++ jz_offer/20210427_BinaryTree/jz24_the_path_with_sum_k_in_the_binary_tree.cpp
@@ -8,6 +8,23 @@ struct TreeNode {
 	}
 };*/
 
+/* solutions
+pathSum(root, sum) 函数：
+
+初始化： 结果列表 res ，路径列表 path 。
+返回值： 返回 res 即可。
+recur(root, tar) 函数：
+
+递推参数： 当前节点 root ，当前目标值 tar 。
+终止条件： 若节点 root 为空，则直接返回。
+递推工作：
+路径更新： 将当前节点值 root.val 加入路径 path ；
+目标值更新： tar = tar - root.val（即目标值 tar 从 sum 减至 00 ）；
+路径记录： 当 ① root 为叶节点 且 ② 路径和等于目标值 ，则将此路径 path 加入 res 。
+先序遍历： 递归左 / 右子节点。
+路径恢复： 向上回溯前，需要将当前节点从路径 path 中删除，即执行 path.pop() 。
+*/
+
 void dfs(TreeNode* root, int num, vector<vector<int>>& res, vector<int>& tmp, int sum){
 //***dfs中的函数vector等容器一定要取引用，最终的值不会更着函数的值而变化
     if(root==NULL){
